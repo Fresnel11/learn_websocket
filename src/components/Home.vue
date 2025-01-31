7999a7e7f5b091b4e14df96c7559c468251b8aea
Fresnel11 <fresneljeanclaude@outlook.com>
2025-01-31 14:12:33 +0100

    fresnel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="text-3xl font-bold text-teal-400 underline cursor-pointer">Hello Tailwind CSS!</div>
-  <UserPen :size="32" class="inline-block ml-2" />
+  <!-- <div class="text-3xl font-bold text-teal-400 underline cursor-pointer"> Hello Tailwind CSS!</div> -->
+  <!-- <UserPen :size="50" /> -->
+  <div class="flex gap-4 w-full h-screen">
+    <div class="w-[25%] border-2 border-red-500">
+      <ToolbarComponent />
+    </div>
+    <div class="flex-1 border-2 border-red-500 w-[75%]">
+      <CanvasComponent />
+    </div>
+  </div>
 </template>
+
 <script setup lang="ts">
-import { ref } from 'vue';
-import {UserPen} from 'lucide-vue-next';
+import { UserPen } from 'lucide-vue-next';
+import ToolbarComponent from './ToolbarComponent.vue';
+import CanvasComponent from './CanvasComponent.vue';
+
 </script>
-<style scoped>
+
+<style>
 
 </style>
---
+++ src/components/ToolbarComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-white rounded-lg shadow-lg px-4 space-y-4 h-full">
+    <div class="flex gap-2 justify-center">
+      <button title="Pinceau">
+        <Paintbrush :size="30" />
+      </button>
+      <button title="Gomme">
+        <Eraser :size="30" />
+      </button>
+    </div>
+    <div>
+      <label for="">Couleur</label>
+      <input type="color" name="" id="">
+    </div>
+    <div>
+      <label for="">Epaisseur</label>
+      <input type="range" min="1" max="50" name="" id="">
+    </div>
+    <div class="flex-1 flex-row mb-1 h-full">
+      <button class="flex gap-2 bg-red-100 text-red-500" title="Nettoyer">
+        <RotateCcw :size="20" color="red" />
+        Nettoyer
+      </button>
+      <button class="flex gap-2 bg-blue-100 text-blue-500" title="Enregistrer">
+        <Download :size="20" />
+        Enregistrer
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Paintbrush, Eraser, RotateCcw, Download } from 'lucide-vue-next';
+
+</script>
+
+<style>
+
+</style>
